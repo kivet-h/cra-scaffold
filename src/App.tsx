1c62dfa12f78fa1520cd844897b9753f9a1ae495
kivet <wanghuixiang@druid.tech>
2023-03-17 13:57:43 +0800

    feat: 项目初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import routes from './route.config'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <Routes>
+          {routes.map((route: any) => (
+            <Route key={route.path} path={route.path} element={route.element}>
+              {route.children &&
+                route.children.map((child: any) => (
+                  <Route
+                    key={`${route.path}/${child.path}`}
+                    path={child.path}
+                    element={child.element}
+                  />
+                ))}
+            </Route>
+          ))}
+        </Routes>
+      </HashRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
